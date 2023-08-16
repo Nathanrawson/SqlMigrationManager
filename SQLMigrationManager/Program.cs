5c0f42921ef2a3b54add336a789ca3d7d420043c
NathanRawson <nathan@push.live>
2023-08-16 15:57:52 +0100

    first commit

---
+++ SQLMigrationManager/Program.cs
@@ -0,0 +1,16 @@
+﻿using SQLMigrationManager.SQLMigrations;
+using System;
+
+namespace SQLMigrationManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spScripts = new StoredProcedureScripts();
+            Console.WriteLine("Running stored procedure scripts");
+            spScripts.Run();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SQLMigrationManager/StoredprocedureScripts.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SQLMigrationManager
+{
+
+    namespace SQLMigrations
+    {
+        public class StoredProcedureScripts
+        {
+            private readonly string connectionString = Environment.GetEnvironmentVariable("connectionString");
+            private readonly string pathOfFiles = Environment.GetEnvironmentVariable("storedProcedureDirectory");
+            public void Run()
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                Server server = new Server(new ServerConnection(connection));
+                List<string> filePaths = Directory.GetFiles(pathOfFiles).ToList();
+                foreach (string file in filePaths)
+                {
+                    try
+                    {
+                        Console.WriteLine($"executing {file}");
+                        FileInfo fileInfo = new FileInfo(file);
+                        string script = fileInfo.OpenText().ReadToEnd();
+                        server.ConnectionContext.ExecuteNonQuery(script);
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine(exception);
+                    }
+                }
+                connection.Close();
+            }
+        }
+    }
+}
